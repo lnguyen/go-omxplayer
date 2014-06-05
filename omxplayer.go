0b1bc6624a3ba01c75a714675ab56af378b8e05c
Long Nguyen <long.nguyen11288@gmail.com>
2014-06-05 14:46:57 -0400

    added check for new connection

--- omxplayer.go
+++ omxplayer.go
@@ -1,6 +1,8 @@
 package omxplayer
 
 import (
+	"io/ioutil"
+	"os"
 	"os/exec"
 
 	dbus "github.com/hoffoo/go.dbus"
@@ -10,7 +12,7 @@ var sdbus *dbus.Object
 
 type OmxPlayer struct {
 	sdbus   *dbus.Object
-	command exec.Cmd
+	command *exec.Cmd
 }
 
 func New() OmxPlayer {
@@ -19,10 +21,15 @@ func New() OmxPlayer {
 
 func (o *OmxPlayer) PlayFile(filename string) error {
 	o.command = exec.Command("omxplayer", "-o", "hdmi", filename)
-	err := o.command.Run()
+	err := o.command.Start()
 	if err != nil {
 		return err
 	}
+	env, err := ioutil.ReadFile("/tmp/omxplayerdbus")
+	if err != nil {
+		return err
+	}
+	os.Setenv("DBUS_SESSION_BUS_ADDRESS", string(env))
 	return nil
 }
 
@@ -32,16 +39,14 @@ func (o *OmxPlayer) PlayPause() error {
 }
 
 func (o *OmxPlayer) Method(method string) error {
-	if o.sdbus == nil {
-		o.sdbus = connDbus()
-	}
+	o.sdbus = connDbus()
 
 	c := o.sdbus.Call(method, 0)
 	return c.Err
 }
 
 func connDbus() *dbus.Object {
-	conn, err := dbus.SessionBusPrivate()
+	conn, err := dbus.SessionBus()
 
 	// couldnt connect to session bus
 	if err != nil {
