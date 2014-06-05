6c8cfcb2842351101c414ffd59d49a5934dade8b
Long Nguyen <long.nguyen11288@gmail.com>
2014-06-05 14:32:28 -0400

    added player

--- omxplayer.go
+++ omxplayer.go
@@ -1,11 +1,42 @@
 package omxplayer
 
-import dbus "github.com/hoffoo/go.dbus"
+import (
+	"os/exec"
+
+	dbus "github.com/hoffoo/go.dbus"
+)
 
 var sdbus *dbus.Object
 
-func connDbus() *dbus.Object {
+type OmxPlayer struct {
+	sdbus   *dbus.Object
+	command exec.Cmd
+}
 
+func (o *OmxPlayer) PlayFile(filename string) error {
+	o.command = exec.Command("omxplayer", "-o", "hdmi", filename)
+	err := o.command.Run()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (o *OmxPlayer) PlayPause() error {
+	err := o.Method("PlayPause")
+	return err
+}
+
+func (o *OmxPlayer) Method(method string) error {
+	if o.sdbus == nil {
+		o.sdbus = connDbus()
+	}
+
+	c := o.sdbus.Call(method, 0)
+	return c.Err
+}
+
+func connDbus() *dbus.Object {
 	conn, err := dbus.SessionBusPrivate()
 
 	// couldnt connect to session bus
@@ -14,27 +45,4 @@ func connDbus() *dbus.Object {
 	}
 
 	return conn.Object("org.mpris.MediaPlayer2.omxplayer", "/org/mpris/MediaPlayer2")
-}
-
-func Status() *dbus.Variant {
-	if sdbus == nil {
-		sdbus = connDbus()
-	}
-
-	pstatus, err := sdbus.GetProperty("org.mpris.MediaPlayer2.Player.PlaybackStatus")
-
-	if err != nil {
-		panic(err) // most likely omxplayer not running
-	}
-
-	return &pstatus
-}
-
-func OmxPlayerMethod(method string) error {
-	if sdbus == nil {
-		sdbus = connDbus()
-	}
-
-	c := sdbus.Call(method, 0)
-	return c.Err
 }
